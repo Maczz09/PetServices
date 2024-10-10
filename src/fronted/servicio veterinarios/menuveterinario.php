32e78984f93d974d09c751621356e3e3968a14a8
H1j0d3D10s@ <jadasane82@gmail.com>
2024-10-10 11:04:00 -0500

    Descripción de los cambios

---
+++ src/fronted/servicio veterinarios/menuveterinario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php 
+include '../html/header.php';
+?>
+<html lang="en">
+    <head>
+    
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        
+        <!-- Favicon-->
+        <!-- Core theme CSS (includes Bootstrap)-->
+        
+        <link rel="stylesheet" href="../css/styles2.css">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <footer>
+            <?php include '../html/footer.php' ?>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="main.js"></script>
+    </body>
+</html>

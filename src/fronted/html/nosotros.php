d40f56d028a1a14b47f6993ba66a803f31f2ff1e
Ignacio C <ignacio.caldsil@gmail.com>
2024-10-16 23:11:31 -0500

    modificacion de nosotros.php

--- src/fronted/html/nosotros.php
+++ src/fronted/html/nosotros.php
@@ -9,18 +9,18 @@
     <title>Nosotros</title>
     <!-- Tailwind CSS Link -->
     <link href="../../output.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
     <!-- Tailwind CSS Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
     <!-- Fontawesome -->
     <script src="https://kit.fontawesome.com/a23e6feb03.js"></script>
-    <link rel="shortcut icon" href="src/img/favicon-32x32.png" type="image/x-icon" sizes="32x32">
     <!-- SWIPER JS SCRIPT -->
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js" defer></script>
 
     <!-- TEMPORARILY USING GOOGLE FONTS FROM SERVER -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@500&display=swap" rel="stylesheet"> -->
 </head>
 
 <body>
@@ -31,7 +31,7 @@
         
             <section class="relative w-full">
                 <div class="w-full mt-16 h-72 bg-cover bg-center"
-                    style="background-image: url('../images/imagen-nosotros.webp');">
+                    style="background-image: url('../images/nosotros/imagen-nosotros.webp');">
                     <div class="absolute inset-0 bg-black opacity-50"></div>
                     <div class="absolute inset-0 flex justify-center items-center">
                         <h1 class="text-white text-5xl md:text-7xl font-bold">Nosotros</h1>
@@ -40,7 +40,7 @@
             </section>
         
         <!-- Sección de contenido e imagen 01" -->
-       
+
         <section
             class="py-5 md:py-10 px-4 md:px-20 bg-gradient-to-r from-blue-50 from-50% via-blue-50 via-30% to-blue-100 to-50%  ">
 
@@ -50,27 +50,21 @@
                 <!-- LEFT COLUMN -->
                 <div class="flex flex-col mb-2 space-y-12 md:mb-32 md:w-1/2">
                     <h1 class="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left text-blue-800">Ellos se
-                        merecen
-                        lo mejor</h1>
+                        merecen lo mejor</h1>
                     <p class="max-w-sm text-center text-darkGrayishBlue sm:mx-auto lg:mx-0 md:text-left text-blue-950">
-                        Por eso es que queremos
-                        mostrarte todas las opciones
-                        posibles que tienen tus mascotas para recibir la mejor atención por parte de profesionales de
-                        calidad y
-                        100% confiables.<br>
+                        Por eso es que queremos mostrarte todas las opciones posibles que tienen tus mascotas para recibir la 
+                        mejor atención por parte de profesionales de calidad y 100% confiables.<br>
                         Y recuerda que...cada tip, consejo, lugar, experiencia y sentimiento es un pequeño aporte que en
-                        su
-                        conjunto transporta a tu mascota
-                        a una nueva y espectacular vivencia para su vida.</p>
+                        su conjunto transporta a tu mascota a una nueva y espectacular vivencia para su vida.</p>
                     <!-- <div class="button flex justify-center md:justify-start">
-              <a href="#"
+            <a href="#"
                 class="cta-button p-3 px-6 pt-2 text-veryLightGray bg-brightRed rounded-full baseline transition ease-in drop-shadow-2xl shadow-brightRed hover:bg-brightRedLight">Get
                 Started</a>
             </div> -->
                 </div>
                 <!-- RIGHT COLUMN -->
                 <div class="md:w-1/2">
-                    <img src="../images/perros01.png" alt="imagen de perro y gato generado con IA">
+                    <img src="../images/nosotros/perros01.png" alt="imagen de perro y gato generado con IA">
                 </div>
             </div>
         </section>
@@ -86,10 +80,8 @@
                     </h2>
                     <p class="max-w-sm text-center text-darkGrayishBlue lg:mx-0 sm:mx-auto md:text-left text-blue-950">
                         Nuestra Misión es que toda mascota viva feliz y sana. Nosotros sabemos que no es una tarea
-                        fácil,
-                        por eso queremos brindarte toda la información posible sobre diversos servicios para mascotas
-                        que harán
-                        posible lograr su bienestar integral.
+                        fácil, por eso queremos brindarte toda la información posible sobre diversos servicios para mascotas
+                        que harán posible lograr su bienestar integral.
                         <br><br>Como equipo queremos mejorar cada día practicando siempre nuestro 3 principales valores:
                     </p>
                 </div>
@@ -174,111 +166,42 @@
             </div>
         </section>
 
+        <!-- SECTION DE UBICACION -->
+        <section class="bg-white bg-gradient-to-r from-blue-100 from-30% via-blue-100 via-30% to-blue-200 to-90%">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+                <div class="mx-auto max-w-screen-sm">
+                    <h2 class="mb-4 text-5xl tracking-tight font-extrabold text-blue-900">Ubicación</h2>
+                    <p class="mb-8 font-light lg:mb-4 sm:text-xl text-blue-950">
+                        ¡Ven con tu mascota querida a nuestra tienda física, nosotros te brindamos la mejor atención!</p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3156.056529599294!2d-80.64119993147855!3d-5.182512845939182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x904a1bdf91d8b825%3A0xa4bfa3480b1255ea!2sUniversidad%20Tecnol%C3%B3gica%20Del%20Per%C3%BA!5e0!3m2!1ses!2spe!4v1729006024112!5m2!1ses!2spe" 
+                            width="700" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <!-- <div id="map" class="w-full h-96"></div>
+                    <script src="../js/script_ubicacion.js"></script>
+                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script> -->
+                </div>
+            </div>
+        </section>
+
         <!-- SECTION TESTIMONIOS-->
+        
         <section class="bg-white bg-gradient-to-r from-blue-100 from-30% via-blue-100 via-30% to-blue-200 to-90%"
             id="testimonios">
             <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
                 <div class="mx-auto max-w-screen-sm">
                     <h2 class="mb-4 text-5xl tracking-tight font-extrabold text-blue-900">Testimonios</h2>
-                    <p class="mb-8 font-light lg:mb-16 sm:text-xl text-blue-950">
+                    <p class=" font-light  sm:text-xl text-blue-950">
                         Nuestros usuarios recomiendan el trabajo que realizamos. Y tú, ¿ya adquiriste los servicio de
                         PetServices?</p>
-                </div>
-                <div class="grid mb-8 lg:mb-12 lg:grid-cols-2">
-                    <figure
-                        class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-blue-600 dark:border-blue-900">
-                        <blockquote class="mx-auto mb-8 max-w-2xl text-white dark:text-white">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Fácil y Organizado</h3>
-                            <p class="my-4">"Me encanta la organización de la página, es muy fácil encontrar los
-                                servicios que necesito para mis mascotas.
-                                La sección de PetFriendly es super útil, ¡Me ha llamado mucho la atención y la estoy
-                                recomendando a toda mi familia y amigos!"</p>
-                        </blockquote>
-                        <figcaption class="flex justify-center items-center space-x-3">
-                            <img class="w-9 h-9 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
-                                alt="usuario mujer">
-                            <div class="space-y-0.5 font-medium dark:text-white text-left">
-                                <div>Laura García</div>
-                                <div class="text-sm font-light text-white dark:text-white">Usuaria de PetServices</div>
-                            </div>
-                        </figcaption>
-                    </figure>
-                    <figure
-                        class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 dark:bg-blue-600 dark:border-blue-900">
-                        <blockquote class="mx-auto mb-8 max-w-2xl text-white dark:text-white">
-                            <h3 class="text-lg font-semibold text-white dark:text-white">Experiencia Agradable y
-                                Satisfactoria</h3>
-                            <p class="my-4">"Utilizar esta página ha sido una experiencia bastante satisfactoria. Pude
-                                reservar la consulta de mi gato sin problemas.
-                                También me llamó la atención que ofrecen una tienda en línea con productos recomendados
-                                por veterinarios,
-                                lo que me dio más confianza para comprar algunos suplementos."</p>
-                        </blockquote>
-                        <figcaption class="flex justify-center items-center space-x-3">
-                            <img class="w-9 h-9 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png"
-                                alt="usuario mujer">
-                            <div class="space-y-0.5 font-medium dark:text-white text-left">
-                                <div>María Saldarriaga</div>
-                                <div class="text-sm font-light text-white dark:text-white">Usuaria de PetServices</div>
-                            </div>
-                        </figcaption>
-                    </figure>
-                    <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r 
-                              dark:bg-blue-600 dark:border-blue-950">
-                        <blockquote class="mx-auto mb-8 max-w-2xl text-white dark:text-white">
-                            <h3 class="text-lg font-semibold text-white dark:text-white">Atractivo y Moderno</h3>
-                            <p class="my-4">"La página de esta veterinaria tiene un diseño atractivo y moderno, lo que
-                                la hace agradable a la vista. Pude encontrar
-                                rápidamente la información sobre los servicios que ofrecen y la atención en las
-                                veterinarias es inmediata lo cual es muy importante para mí porque tengo
-                                una perrita mayor que requiere cuidados especiales."</p>
-                        </blockquote>
-                        <figcaption class="flex justify-center items-center space-x-3">
-                            <img class="w-9 h-9 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                                alt="usuario hombre">
-                            <div class="space-y-0.5 font-medium dark:text-white text-left">
-                                <div>Pedro Quintana</div>
-                                <div class="text-sm font-light text-white dark:text-white">Usuario de PetServices</div>
-                            </div>
-                        </figcaption>
-                    </figure>
-                    <figure
-                        class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12 dark:bg-blue-600 dark:border-blue-950">
-                        <blockquote class="mx-auto mb-8 max-w-2xl text-white dark:text-white">
-                            <h3 class="text-lg font-semibold text-white dark:text-white">Completa y Profesional</h3>
-                            <p class="my-4">"Ofrecen una variedad de servicios muy bien explicados, desde cirugías hasta
-                                asesoramiento para mi mascota.
-                                También tienen un blog con artículos muy actualizados sobre temas de salud animal, lo
-                                cual aprecio mucho porque me gusta
-                                informarme sobre los cuidados que necesita mi perro."</p>
-                        </blockquote>
-                        <figcaption class="flex justify-center items-center space-x-3">
-                            <img class="w-9 h-9 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
-                                alt="usuario hombre">
-                            <div class="space-y-0.5 font-medium dark:text-white text-left">
-                                <div>Max García</div>
-                                <div class="text-sm font-light text-white dark:text-white">Usuario de PetServices</div>
-                            </div>
-                        </figcaption>
-                    </figure>
-                </div>
-                <div class="text-center">
-                    <a href="#" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-blue-900 
-                focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4
-                focus:ring-gray-200 dark:focus:ring-blue-700 dark:bg-blue-800 dark:text-white dark:border-blue-600 dark:hover:text-white
-                dark:hover:bg-blue-700 dark:hover:border-blue-950">Leer más</a>
+                    <?php
+                        include '../nosotros/testimonios.php';
+                    ?>
                 </div>
         </section>
+        <!-- Incluir el footer -->
         <?php include 'footer.php'; ?>
+        <script src="../js/main.js"></script>
     </section>
     </main>
-    
-    <script src="ocultar_mostrar.js">
-    </script>
 
 </body>
 

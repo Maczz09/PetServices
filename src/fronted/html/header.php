1dd862112f1bd8058b96c7f9a51fe58b97ad9101
Ignacio C <ignacio.caldsil@gmail.com>
2024-12-02 08:28:36 -0500

    quitando actualizacion  manual de carpeta de nosotros y mejora de ruta en header

--- src/fronted/html/header.php
+++ src/fronted/html/header.php
@@ -46,7 +46,7 @@ include '../../backend/config/session.php';
                     class="smky-btn3 relative hover:text-white py-2 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 after:transition-all after:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-[#278093] after:rounded-t-full after:w-full after:bottom-0 after:left-0 text-[#132f39]">
                     Servicios
                 </a>
-                <a href="http://localhost/PetServices/src/fronted/html/nosotros.php"
+                <a href="../html/nosotros.php"
                     class="smky-btn3 relative hover:text-white py-2 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 after:transition-all after:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-[#278093] after:rounded-t-full after:w-full after:bottom-0 after:left-0 text-[#132f39]">
                     Nosotros
                 </a>
